e8a264bb3918efbeb2ccdea0db0aef47fb18e5cf
hoeftale <103036671+hoeftale@users.noreply.github.com>
2022-05-12 12:09:54 +0200

    Update project-factory module version

--- showcase/dev/main.tf
+++ showcase/dev/main.tf
@@ -4,14 +4,14 @@ data "opentelekomcloud_identity_project_v3" "current" {}
 module "cloud_tracing_service" {
   providers    = { opentelekomcloud = opentelekomcloud.top_level_project }
   source       = "iits-consulting/project-factory/opentelekomcloud//modules/cloud_tracing_service"
-  version      = "4.0.0"
+  version      = "4.0.1"
   bucket_name  = replace(lower("${data.opentelekomcloud_identity_project_v3.current.name}-${var.context}-${var.stage}-cts"), "_", "-")
   project_name = data.opentelekomcloud_identity_project_v3.current.name
 }
 
 module "vpc" {
   source     = "iits-consulting/project-factory/opentelekomcloud//modules/vpc"
-  version    = "4.0.0"
+  version    = "4.0.1"
   name       = "${var.context}-${var.stage}-vpc"
   tags       = local.tags
   cidr_block = var.vpc_cidr
@@ -22,7 +22,7 @@ module "vpc" {
 
 module "cce" {
   source  = "iits-consulting/project-factory/opentelekomcloud//modules/cce"
-  version = "4.0.0"
+  version = "4.0.1"
   name    = "${var.context}-${var.stage}"
 
   cluster_config = {
@@ -47,7 +47,7 @@ module "cce" {
 
 module "loadbalancer" {
   source       = "iits-consulting/project-factory/opentelekomcloud//modules/loadbalancer"
-  version      = "4.0.0"
+  version      = "4.0.1"
   context_name = var.context
   subnet_id    = module.vpc.subnets["${var.context}-${var.stage}-subnet"].subnet_id
   stage_name   = var.stage
@@ -56,7 +56,7 @@ module "loadbalancer" {
 
 module "private_dns" {
   source  = "iits-consulting/project-factory/opentelekomcloud//modules/private_dns"
-  version = "4.0.0"
+  version = "4.0.1"
   domain  = "internal.${var.context}.de"
   a_records = {
     example = ["192.168.0.0"]
@@ -67,7 +67,7 @@ module "private_dns" {
 module "encyrpted_secrets_bucket" {
   providers         = { opentelekomcloud = opentelekomcloud.top_level_project }
   source            = "iits-consulting/project-factory/opentelekomcloud//modules/obs_secrets_writer"
-  version           = "4.0.0"
+  version           = "4.0.1"
   bucket_name       = replace(lower("${data.opentelekomcloud_identity_project_v3.current.name}-${var.context}-${var.stage}-stage-secrets"), "_", "-")
   bucket_object_key = "terraform-secrets"
   secrets = {

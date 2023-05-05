6585eb9a22e343ebd26371214d90ec18094cd4ce
Victor Getz <sd>
2023-05-05 13:51:14 +0200

    Bugfix Environment variables, tfstate backend, argo version

--- otc-cloud/dev/kubernetes/settings.tf
+++ otc-cloud/dev/kubernetes/settings.tf
@@ -2,14 +2,7 @@ terraform {
   required_version = "v1.3.5"
 
   backend "s3" {
-    bucket = "eu-de-victor-dev-tfstate"
-    kms_key_id = "arn:aws:kms:eu-de:d32336fe26ec415caa04e17e9adfb6a8:key/12077fa3-582f-4757-a078-f704c3e9a501"
-    key = "tfstate-kubernetes"
-    region = "eu-de"
-    endpoint = "obs.eu-de.otc.t-systems.com"
-    encrypt = true
-    skip_region_validation = true
-    skip_credentials_validation = true
+    //TODO
   }
   required_providers {
     opentelekomcloud = {

6305fb7ee263c0d355c8981eb45fe01bc0da4669
Mweya <mweya.ruider@iits-consulting.de>
2025-05-13 17:09:13 +0200

    fix(crds): Downgrade version
    7.5.0 either doesn't exist or isn't supported on tf 1.9.0

--- otc-cloud/dev/10_crds/crds.tf
+++ otc-cloud/dev/10_crds/crds.tf
@@ -1,6 +1,6 @@
 module "crds" {
   source     = "registry.terraform.io/iits-consulting/project-factory/opentelekomcloud//modules/crd_installer"
-  version    = "7.5.0"
+  version    = "7.4.1"
   apply_only = false
   default_chart_overrides = {
     traefik = {
